10f86082908f52051de0deb4f5cddabb7c15d679
WILLBER-cod <66934794+WILLBER-cod@users.noreply.github.com>
2020-08-27 12:06:45 +0800

    init

---
+++ New Unity Project/Assets/Script/Signaling.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Signaling : MonoBehaviour
+{
+    private AudioSource _audio;
+
+    private void Start()
+    {
+        _audio = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<PlayerController>(out PlayerController player))
+        {
+              _audio.Play();
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<PlayerController>(out PlayerController player))
+        {
+            _audio.volume += 0.01f;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<PlayerController>(out PlayerController player))
+        {
+            _audio.volume = 0;
+
+            _audio.Stop();
+        }
+    }
+}
+
