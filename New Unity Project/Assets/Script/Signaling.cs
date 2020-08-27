23a6a78bebe6d587119a7fc28a25c0653f13e181
WILLBER-cod <66934794+WILLBER-cod@users.noreply.github.com>
2020-08-27 19:46:41 +0800

    lol

--- New Unity Project/Assets/Script/Signaling.cs
+++ New Unity Project/Assets/Script/Signaling.cs
@@ -2,38 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
+
 public class Signaling : MonoBehaviour
 {
     private AudioSource _audio;
+
+    private bool _exit = false;
+    private bool _enter = false;
 
     private void Start()
     {
         _audio = GetComponent<AudioSource>();
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void Update()
     {
-        if (collision.TryGetComponent<PlayerController>(out PlayerController player))
-        {
-              _audio.Play();
-        }
+        if (_exit == true && _audio.volume != 0)
+            _audio.volume -= 0.01f;
+
+        else if (_enter == true && _audio.volume != 1)
+            _audio.volume += 0.005f;
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.TryGetComponent<PlayerController>(out PlayerController player))
+        if (collision.TryGetComponent<PlayerMovement>(out PlayerMovement player))
         {
-            _audio.volume += 0.01f;
+            _audio.Play();
+
+            _exit = false;
+            _enter = true;
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.TryGetComponent<PlayerController>(out PlayerController player))
+        if (collision.TryGetComponent<PlayerMovement>(out PlayerMovement player))
         {
-            _audio.volume = 0;
-
-            _audio.Stop();
+            _exit = true;
+            _enter = false;
         }
     }
 }
