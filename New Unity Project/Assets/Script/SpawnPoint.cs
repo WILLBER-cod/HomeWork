11bc1a678f4f45110b66b76d206af515c6b398ed
WILLBER-cod <66934794+WILLBER-cod@users.noreply.github.com>
2020-08-31 18:37:30 +0800

    commit

---
+++ New Unity Project/Assets/Script/SpawnPoint.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    [SerializeField] private Transform _path;
+    [SerializeField] private GameObject _target;
+
+    private Transform[] _points;
+
+    private float _time;
+
+    private int _index;
+
+    private void Start()
+    {
+        _points = new Transform[_path.childCount];
+  
+        for(int i = 0; i < _path.childCount; i++)
+        {
+            _points[i] = _path.GetChild(i);
+        }
+    }
+
+    private void Update()
+    {
+        _time += Time.deltaTime;
+        if(_time >= 2 && _index != _path.childCount)
+        {
+            Spawn();
+
+            _time = 0;
+
+            _index++;
+        }
+    }
+
+    void Spawn()
+    {
+        Transform Target = _points[_index];
+        Instantiate(_target, transform.position = Target.position, Quaternion.identity);
+    }
+
+}
